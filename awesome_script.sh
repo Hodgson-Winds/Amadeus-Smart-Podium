34f1927d678f4e6bc0123b959b20f28a21782fae
Caleb Adams <piepieninja@gmail.com>
2015-04-15 00:05:15 -0400

    adds the basic configure script

---
+++ awesome_script.sh
@@ -0,0 +1,9 @@
+#Caleb is AWESOME!!
+# xrandr command will show all monitors
+# xinput command will show all inputs
+
+#fix touch inputs
+xinput map-to-output 11 HDMI1
+xinput map-to-output 12 VGA1
+
+# optional to run xinput_calibrator to fix off by a few touch errors
